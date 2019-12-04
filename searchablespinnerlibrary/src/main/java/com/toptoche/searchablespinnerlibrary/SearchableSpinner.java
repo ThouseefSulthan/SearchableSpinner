06849a9f7d96facae3b16f380c20bb8afc0089fd
ThouseefSulthan <thouseefsulthan@gmail.com>
2019-12-04 16:18:21 +0530

    Update SearchableSpinner.java

--- searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableSpinner.java
+++ searchablespinnerlibrary/src/main/java/com/toptoche/searchablespinnerlibrary/SearchableSpinner.java
@@ -90,8 +90,9 @@ public class SearchableSpinner extends Spinner implements View.OnTouchListener,
                     _items.add(_arrayAdapter.getItem(i));
                 }
                 // Change end.
-
+if(!_searchableListDialog.isAdded()) {
                 _searchableListDialog.show(scanForActivity(_context).getFragmentManager(), "TAG");
+}
             }
         }
         return true;
